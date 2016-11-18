11a2a06955f49531361c89f265dea3e6c87cc431
David Hong <david.hong42@gmail.com>
2016-11-18 10:49:54 -0500

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var fs = require('fs');
 var cors = require('cors');
 
+// ---- APP SETUP ----- //
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
@@ -19,31 +20,18 @@ app.listen(app.get('port'), '0.0.0.0', function() {
   console.log("server starting on " + app.get('port'));
 });
 
+// ---- BLOCKCHAIN STUFF ----- //
 var Ibc1 = require('ibm-blockchain-js');														//rest based SDK for ibm blockchain
 var ibc = new Ibc1();
+var routes = require('./routes')
 
-try{
-	var manual = JSON.parse(fs.readFileSync('mycreds.json', 'utf8'));
-	var peers = manual.credentials.peers;
-	console.log('loading hardcoded peers');
-	var users = null;																			//users are only found if security is on
-	if(manual.credentials.users) users = manual.credentials.users;
-	console.log('loading hardcoded users');
-}
-catch(e){
-	console.log('Error - could not find hardcoded peers/users, this is okay if running in bluemix');
-}
+var manual = JSON.parse(fs.readFileSync('mycreds.json', 'utf8'));
+var peers = manual.credentials.peers;
+var users = manual.credentials.users;
 
 function prefer_type1_users(user_array){
-	var ret = [];
-	for(var i in users){
-		if(users[i].enrollId.indexOf('type1') >= 0) {	//gather the type1 users
-			ret.push(users[i]);
-		}
-	}
-
-	if(ret.length === 0) ret = user_array;				//if no users found, just use what we have
-	return ret;
+	var ret = user_array.filter(o => o.enrollId.indexOf('type1') >= 0);
+	return ret.length === 0 ? user_array : ret;  //if no users found, just use what we have
 }
 
 var options = 	{
@@ -51,36 +39,37 @@ var options = 	{
 		peers: peers,																	//lets only use the first peer! since we really don't need any more than 1
 		users: prefer_type1_users(users),											//dump the whole thing, sdk will parse for a good one
 		options: {
-					quiet: true 															//detailed debug messages on/off true/false
-				}
+			quiet: true 															//detailed debug messages on/off true/false
+		}
 	},
 	chaincode:{
-		zip_url: 'https://github.com/ibm-blockchain/marbles/archive/v2.0.zip',
-		unzip_dir: 'marbles-2.0/chaincode',													//subdirectroy name of chaincode after unzipped
-		git_url: 'http://gopkg.in/ibm-blockchain/marbles.v2/chaincode',		//GO get http url
+		zip_url: 'https://github.com/david-hong/blockchain-test/raw/master/chaincode.zip',
+		unzip_dir: 'chaincode',													//subdirectroy name of chaincode after unzipped
+		git_url: 'http://gopkg.in/david-hong/blockchain-test.v1/chaincode',		//GO get http url
 	}
 };
 
 var chaincode = null;
+
 ibc.load(options, function (err, cc){														//parse/load chaincode, response has chaincode functions!
 	if(err != null){
 		console.log('! looks like an error loading the chaincode or network, app will fail\n', err);
-		if(!process.error) process.error = {type: 'load', msg: err.details};				//if it already exist, keep the last error
+    throw(err)
 	}
-	else{
-		chaincode = cc;
-		// PASS ANY CC OBJECTS TO WS
 
-		// ---- To Deploy or Not to Deploy ---- //
-		if(!cc.details.deployed_name || cc.details.deployed_name === ''){					//yes, go deploy
-			cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
-				check_if_deployed(e, 1);
-			});
-		}
-		else{																				//no, already deployed
-			console.log('chaincode summary file indicates chaincode has been previously deployed');
-			check_if_deployed(null, 1);
-		}
+	chaincode = cc;
+	// PASS ANY CC OBJECTS TO WS
+  routes.setup(ibc, cc)
+
+	// ---- To Deploy or Not to Deploy ---- //
+	if(!cc.details.deployed_name || cc.details.deployed_name === ''){					//yes, go deploy
+		cc.deploy('init', ['99'], {delay_ms: 30000}, function(e){ 						//delay_ms is milliseconds to wait after deploy for conatiner to start, 50sec recommended
+			check_if_deployed(e, 1);
+		});
+	}
+	else{																				//no, already deployed
+		console.log('chaincode summary file indicates chaincode has been previously deployed');
+		check_if_deployed(null, 1);
 	}
 });
 
@@ -96,18 +85,20 @@ function check_if_deployed(e, attempt){
 	}
 	else{
 		console.log('[preflight check]', attempt, ': testing if chaincode is ready');
-		chaincode.query.read(['_marbleindex'], function(err, resp){
+		chaincode.query.read(['test'], function(err, resp){
 			var cc_deployed = false;
+
 			try{
 				if(err == null){															//no errors is good, but can't trust that alone
-					if(resp === 'null') cc_deployed = true;									//looks alright, brand new, no marbles yet
+					if(!resp || resp === 'null') cc_deployed = true;									//looks alright, brand new
 					else{
+            console.log(resp)
 						var json = JSON.parse(resp);
-						if(json.constructor === Array) cc_deployed = true;					//looks alright, we have marbles
+						if(json.constructor === Array) cc_deployed = true;
 					}
 				}
 			}
-			catch(e){}																		//anything nasty goes here
+			catch(e){console.log(e)}																		//anything nasty goes here
 
 			// ---- Are We Ready? ---- //
 			if(!cc_deployed){
@@ -118,9 +109,23 @@ function check_if_deployed(e, attempt){
 			}
 			else{
 				console.log('[preflight check]', attempt, ': success');
-        chaincode.query.read(['a'])
-				//cb_deployed(null);															//yes, lets go!
+        //chaincode.invoke.write(["Sam","500"])
+        //chaincode.invoke.createAccount(["Robby"])
+        //chaincode.query.read(["Bob"])
+				//cb_deployed(null);															//set up ws?
 			}
 		});
 	}
 }
+
+// SETUP ROUTES
+app.use(function(req, res, next) {
+  if(routes.isSetup()){
+    next();
+  }
+  else{
+    console.log("Blockchain not set up in router, cant route")
+  }
+});
+
+app.use('/', routes.router);
