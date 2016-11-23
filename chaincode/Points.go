9381a1b7af5d7f766ba586ae9cb18b911f99702d
David Hong <david.hong42@gmail.com>
2016-11-23 13:34:33 -0500

    Update Points.go

--- chaincode/Points.go
+++ chaincode/Points.go
@@ -216,7 +216,7 @@ func (t *SimpleChaincode) set_user(stub shim.ChaincodeStubInterface, args []stri
   toRes.CashBalance = toRes.CashBalance + transferAmount
   fromRes.CashBalance = fromRes.CashBalance - transferAmount
 
-  transID := uuid.NewV4().String()
+  transID, err := uuid.NewV4().String()
   timestamp := time.Now().Format(time.RFC3339)
   trans := Transaction{ID: transID, Timestamp: timestamp, FromUser: fromRes.ID, ToUser: toRes.ID, Quantity: transferAmount}
   transBytes, err := json.Marshal(&trans)
