4433a063b7eba4a799116c786a34ef5446691bc2
David Hong <david.hong42@gmail.com>
2016-11-23 15:33:27 -0500

    Update Points.go

--- chaincode/Points.go
+++ chaincode/Points.go
@@ -221,8 +221,7 @@ func (t *SimpleChaincode) set_user(stub shim.ChaincodeStubInterface, args []stri
   b := make([]byte, 16)
   _, err = rand.Read(b)
   if err != nil {
-      fmt.Println("Error: ", err)
-      return
+	fmt.Println("Error: ", err)
   }
 
   transID := fmt.Sprintf("%X-%X-%X-%X-%X", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
