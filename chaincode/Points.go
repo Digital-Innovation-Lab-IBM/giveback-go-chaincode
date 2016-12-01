647e860e64966194b70c637a91fc8ecbbeb1e8c4
David Hong <david.hong42@gmail.com>
2016-12-01 16:02:33 -0500

    Update Points.go

--- chaincode/Points.go
+++ chaincode/Points.go
@@ -247,40 +247,6 @@ func (t *SimpleChaincode) PurchaseProduct(stub shim.ChaincodeStubInterface, args
 	return nil, nil
 }
 
-func (t *SimpleChaincode) AddAllowance(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var err error
-  var toRes Account
-	//     0         1
-	// "User",     "500"
-	if len(args) < 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2")
-	}
-
-  username := args[0]
-
-  toAccountAsBytes, err := stub.GetState(username)
-	if err != nil {
-		return nil, errors.New("Failed to get thing")
-	}
-  toRes = Account{}
-	json.Unmarshal(toAccountAsBytes, &toRes)
-
-  transferAmount, err := strconv.Atoi(args[1])
-   if err != nil {
-      // handle error
-   }
-
-  toRes.GiveBalance = toRes.GiveBalance + transferAmount
-
-	toJsonAsBytes, _ := json.Marshal(toRes)
-	err = stub.PutState(username, toJsonAsBytes)								//rewrite the marble with id as key
-	if err != nil {
-		return nil, err
-	}
-
-	return nil, nil
-}
-
 // ============================================================================================================================
 // Set Trade - create an open trade for a marble you want with marbles you have
 // ============================================================================================================================
